b815644eb673050fc6a5289800f061f1b06283c2
shamsher kumawat <shamsher.kumawat@gmail.com>
2022-12-22 22:23:11 +0530

    revert image link changes

--- taxpertUI/src/components/pages/about/AboutPage.js
+++ taxpertUI/src/components/pages/about/AboutPage.js
@@ -1,43 +1,43 @@
 import React from "react";
 import "./AboutPage.css";
-import benefits from "../../../assets/img/benefits.png"
-import transparency from "../../../assets/img/transparency.png"
-import experts from "../../../assets/img/experts.png"
-import tax from "../../../assets/img/tax.jpg"
-import user from "../../../assets/img/user.jpg"
-
-
-
+// import benefits from "../../../assets/img/benefits.png";
+// import transparency from "../../../assets/img/transparency.png";
+// import experts from "../../../assets/img/experts.png";
+// import tax from "../../../assets/img/tax.jpg";
+// import user from "../../../assets/img/user.jpg";
 
 const AboutPage = () => (
   <>
-    
     <div className="mission-head">Our Mission</div>
     <p className="mission">
       Our mission is to simplify finances, save money and time for millions of
       Indian businesses and people.
     </p>
-      <br />
-      <div className="section-1">
-      <h6 className="sec-1-head">Your Tax Prep Comes with Built-in Reassurance</h6>
+    <br />
+    <div className="section-1">
+      <h6 className="sec-1-head">
+        Your Tax Prep Comes with Built-in Reassurance
+      </h6>
       <div className="cards-place">
         <div className="cards-in-place">
           <img
             className="img-place"
-            src={benefits}
+            src="https://cdn-icons-png.flaticon.com/512/1578/1578656.png"
             alt=""
             width="80px"
             height="80px"
           />
           <br />
           <h6 className="headings">Maximum Benefits</h6>
-          <p className="text-area">We ensure that your tax filing is hassle-free with maximum possible
-          tax refund by providing our expert advice at reasonable cost.</p>
+          <p className="text-area">
+            We ensure that your tax filing is hassle-free with maximum possible
+            tax refund by providing our expert advice at reasonable cost.
+          </p>
         </div>
         <div className="cards-in-place">
           <img
             className="img-place"
-            src={transparency}
+            src="https://cdn.iconscout.com/icon/premium/png-256-thumb/audit-2010222-1696751.png"
             alt=""
             width="80px"
             height="80px"
@@ -52,7 +52,7 @@ const AboutPage = () => (
         <div className="cards-in-place">
           <img
             className="img-place"
-            src={experts}
+            src="https://cdn.iconscout.com/icon/premium/png-128-thumb/expert-team-2625507-2178318.png"
             alt=""
             width="80px"
             height="80px"
@@ -64,10 +64,12 @@ const AboutPage = () => (
             expertise for providing best solutions.
           </p>
         </div>
-    </div>
+      </div>
     </div>
     <br />
-    <h6 className="sec-2-heading">From Compliance to Financial Services, We Cover Them All</h6>
+    <h6 className="sec-2-heading">
+      From Compliance to Financial Services, We Cover Them All
+    </h6>
 
     <div className="section-2">
       <div className="about-text">
@@ -79,7 +81,7 @@ const AboutPage = () => (
       </div>
       <img
         className="section-2-image"
-        src={tax}
+        src="https://img.freepik.com/free-vector/businessman-accountant-filling-financial-document-form-clipboard-payment-date-tax-form-income-tax-return-company-tax-payment-concept-illustration_335657-2320.jpg?w=2000"
         alt="extra image"
         width="800px"
         height="500px"
@@ -87,62 +89,62 @@ const AboutPage = () => (
     </div>
     <br />
     <div className="sec-3">
-    <h6 className="sec-3-head">Meet Our Core Team</h6>
-    <hr className="taxpert-line"/>
-    <br />
-    <div className="">
-      <div className="cards-inline">
-        <div className="card">
-          <img
-            src={user}
-            className="card-img-top"
-            alt="user image"
-          />
-          <div className="card-body">
-            <div className="card-text">
-            <h5>Name</h5>
-              <h6>Designation</h6>
-           </div>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            src={user}
-            className="card-img-top"
-            alt="user image"
-          />
-          <div className="card-body">
-            <div className="card-text">
-              <h5>Name</h5>
-              <h6>Designation</h6>
+      <h6 className="sec-3-head">Meet Our Core Team</h6>
+      <hr className="taxpert-line" />
+      <br />
+      <div className="">
+        <div className="cards-inline">
+          <div className="card">
+            <img
+              src="https://t3.ftcdn.net/jpg/05/17/79/88/360_F_517798849_WuXhHTpg2djTbfNf0FQAjzFEoluHpnct.jpg"
+              className="card-img-top"
+              alt="user image"
+            />
+            <div className="card-body">
+              <div className="card-text">
+                <h5>Name</h5>
+                <h6>Designation</h6>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card">
-          <img
-            src={user}
-            className="card-img-top"
-            alt="user image"
-          />
-          <div className="card-body">
-            <div className="card-text">
-            <h5>Name</h5>
-              <h6>Designation</h6>
-           </div>
+          <div className="card">
+            <img
+              src="https://t3.ftcdn.net/jpg/05/17/79/88/360_F_517798849_WuXhHTpg2djTbfNf0FQAjzFEoluHpnct.jpg"
+              className="card-img-top"
+              alt="user image"
+            />
+            <div className="card-body">
+              <div className="card-text">
+                <h5>Name</h5>
+                <h6>Designation</h6>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <img
-            src={user}
-            className="card-img-top"
-            alt="user image"
-          />
-          <div className="card-body">
-            <div className="card-text">
-            <h5>Name</h5>
-              <h6>Designation</h6>
-           </div>
+          <div className="card">
+            <img
+              src="https://t3.ftcdn.net/jpg/05/17/79/88/360_F_517798849_WuXhHTpg2djTbfNf0FQAjzFEoluHpnct.jpg"
+              className="card-img-top"
+              alt="user image"
+            />
+            <div className="card-body">
+              <div className="card-text">
+                <h5>Name</h5>
+                <h6>Designation</h6>
+              </div>
+            </div>
           </div>
+          <div className="card">
+            <img
+              src="https://t3.ftcdn.net/jpg/05/17/79/88/360_F_517798849_WuXhHTpg2djTbfNf0FQAjzFEoluHpnct.jpg"
+              className="card-img-top"
+              alt="user image"
+            />
+            <div className="card-body">
+              <div className="card-text">
+                <h5>Name</h5>
+                <h6>Designation</h6>
+              </div>
+            </div>
           </div>
         </div>
       </div>
