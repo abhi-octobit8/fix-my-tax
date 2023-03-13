c0122408e84b4db33d2f826a3289877f33e8023b
Neeraj Gupta <neeraj1032@gmail.com>
2023-03-14 01:18:40 +0530

    mail setting

---
+++ aspnet-core/src/FixMyTax.Application/FixMyTaxServices/IFMTCustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using FixMyTax.Roles.Dto;
+using FixMyTax.Users.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixMyTax.FixMyTaxServices
+{
+    public interface IFMTCustomerService
+    {
+        Task<UserDto> GetCustomerById(int id);
+    }
+}
