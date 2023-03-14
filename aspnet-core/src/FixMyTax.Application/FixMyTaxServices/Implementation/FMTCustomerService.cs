dfe15751c2d230ebc17b3897df6be89e29508e35
Neeraj Gupta <neeraj1032@gmail.com>
2023-03-14 10:09:59 +0530

    Role fix

--- aspnet-core/src/FixMyTax.Application/FixMyTaxServices/Implementation/FMTCustomerService.cs
+++ aspnet-core/src/FixMyTax.Application/FixMyTaxServices/Implementation/FMTCustomerService.cs
@@ -1,12 +1,16 @@
 ﻿using Abp.Authorization;
 using Abp.Domain.Repositories;
+using Abp.EntityFrameworkCore.Repositories;
+using FixMyTax.Authorization.Roles;
 using FixMyTax.Authorization.Users;
 using FixMyTax.FixMyTaxModels;
 using FixMyTax.FixMyTaxServices.Dtos.Tickets;
 using FixMyTax.Users.Dto;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,16 +20,24 @@ namespace FixMyTax.FixMyTaxServices.Implementation
     public class FMTCustomerService : FixMyTaxAppServiceBase, IFMTCustomerService
     {
         private readonly IRepository<User, long> _userRepository;
+        private readonly RoleManager _roleManager;
 
-        public FMTCustomerService(IRepository<User, long> userRepository)
+        public FMTCustomerService(IRepository<User, long> userRepository, RoleManager roleManager)
         {
             _userRepository = userRepository;
+            _roleManager = roleManager;
         }
 
         public async Task<UserDto> GetCustomerById(int id)
         {
-            var user = await _userRepository.GetAsync(id);
-            return ObjectMapper.Map<UserDto>(user);
+            var user = _userRepository.GetAll().Include(x => x.Roles).Where(x => x.Id == id).FirstOrDefault();
+
+            var roleIds = user.Roles.Select(x => x.RoleId).ToArray();
+            var roles = _roleManager.Roles.Where(r => roleIds.Contains(r.Id)).Select(r => r.NormalizedName);
+            var userDto = ObjectMapper.Map<UserDto>(user);
+            userDto.RoleNames = roles.ToArray();
+
+            return userDto;
         }
     }
 }
