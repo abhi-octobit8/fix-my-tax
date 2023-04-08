ed60092aecab09cb0b76f69a00ef6babd77e0ca3
shamsher kumawat <shamsher.kumawat@gmail.com>
2023-04-08 14:26:01 +0530

    updated id

--- aspnet-core/src/FixMyTax.Application/FixMyTaxServices/Implementation/RegisterService.cs
+++ aspnet-core/src/FixMyTax.Application/FixMyTaxServices/Implementation/RegisterService.cs
@@ -104,7 +104,7 @@ namespace FixMyTax.FixMyTaxServices.Implementation
             _emailSender.Send(
                 to: "karsathi.ajit@gmail.com",
                 subject: "You have a new task!",
-                body: $"A new task is created on the portal  task link: <b>{output.TicketId}</b>",
+                body: $"A new task is created on the portal  task link: <b>{output.id}</b>",
                 isBodyHtml: true
             );
             return output;
